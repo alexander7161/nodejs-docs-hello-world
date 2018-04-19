12436144cb6879a33dea430703d26593145be45f
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-04-19 22:47:24 +0100

    remove

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ var server = http.createServer(function(request, response) {
     response.end("Hello World! Alex");
 
 });
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
